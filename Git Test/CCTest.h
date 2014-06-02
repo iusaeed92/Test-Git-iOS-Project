f2274e0fe63af1ce3c51eeb3ad2a358f5e337f32
Ibrahim Urooj Saeed <ibrahim.urooj@gmail.com>
2014-06-02 12:12:16 -0400

    added a test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Ibrahim Saeed on 6/2/14.
+//  Copyright (c) 2014 Ibrahim Saeed. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
